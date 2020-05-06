1455d43e6a833f722f51cb8bc7ddf374fde62a04
semio <semio@posteo.net>
2020-05-06 11:31:42 +0800

    add functions to extract concepts/datapoints

--- etl/scripts/etl_new.py
+++ etl/scripts/etl_new.py
@@ -12,7 +12,7 @@ import os
 from ddf_utils.str import to_concept_id
 from ddf_utils.io import dump_json
 from ddf_utils.package import get_datapackage
-from ddf_utils.model.ddf import DataPoint, Concept, EntityDomain, Entity
+from ddf_utils.model.ddf import Concept, EntityDomain, Entity
 
 # configuration of file path.
 source_dir = '../source/'
@@ -23,9 +23,6 @@ country_csv = os.path.join(source_dir, 'WDICountry.csv')
 series_csv = os.path.join(source_dir, 'WDISeries.csv')
 groups_xls = os.path.join(source_dir, 'CLASS.xls')
 domain_xls = os.path.join(source_dir, 'wb_economy_entity_domain.xlsx')
-
-CONCEPTS = list()
-DOMAINS = list()
 
 
 def extrace_economy_entities(domains: pd.DataFrame, groups: pd.DataFrame):
@@ -58,7 +55,8 @@ def extrace_economy_entities(domains: pd.DataFrame, groups: pd.DataFrame):
             try:
                 sets = set_membership[g]
             except KeyError:
-                print("warning: group not found: {g}, please add it to the wb_economy_entity_domain.xlsx file.")
+                print("warning: group not found: {g}, please add it to the "
+                      "wb_economy_entity_domain.xlsx file.")
                 raise
             for s in sets:
                 props[s] = to_concept_id(g)
@@ -68,23 +66,93 @@ def extrace_economy_entities(domains: pd.DataFrame, groups: pd.DataFrame):
     return all_entities
 
 
+def remove_cr(df):
+    """escape all carrier returns in each cell"""
+
+    def process(s):
+        if not pd.isnull(s):
+            if isinstance(s, str):
+                return s.replace('\n', '\\n')
+        return s
+
+    for c in df.columns:
+        df[c] = df[c].map(process)
+
+
+def extract_concept_series(series):
+    """extract all concepts from series metadata"""
+
+    df = series.copy()
+    df['concept'] = series['Series Code'].apply(to_concept_id)
+    df = df.set_index('concept')
+
+    # use 'Indicator Name' column as name to indicator concepts
+    df = df.rename(columns={'Indicator Name': 'Name'})
+
+    remove_cr(df)
+
+    # first emit all discrete concepts
+    for column in df.columns:
+        yield Concept(id=to_concept_id(column),
+                      concept_type='string',
+                      props={'name': column})
+
+    # then emit all continuous concepts
+    df.columns = df.columns.map(to_concept_id)
+    for concept, row in df.iterrows():
+        props = row.to_dict()
+        yield Concept(id=concept, concept_type='measure', props=props)
+
+
+def extract_datapoints_country_year(data):
+    """extract all data points by country and year, base on the data csv"""
+
+    res = {}
+
+    # usually data begins from 1960, but we should ensure that.
+    print("Data should begins from 1960. Here are columns we won't include in "
+          "datapoints. Please check if they are correct:")
+    idx = list(data.iloc[0].index).index('1960')
+    print(data.iloc[0].index[:idx])
+
+    # group the data by series.
+    gs = data.groupby(by='Indicator Code').groups
+
+    for subject in data['Indicator Code'].unique():
+        s = to_concept_id(subject)
+        headers_datapoints = ['country', 'year', s]
+
+        data_all = data.ix[gs[subject]].copy()
+
+        data_all['Country Code'] = data_all['Country Code'].apply(to_concept_id)
+        data_all = data_all.set_index('Country Code')
+        data_all = data_all.T['1960':]  # data begins from 1960
+        data_all = data_all.unstack()  # adding back country code as column
+        data_all = data_all.reset_index().dropna()  # ... and year column
+        data_all.columns = headers_datapoints
+
+        res[s] = data_all
+
+    return res
+
+
 def main():
     print("reading source files...")
-    # data = pd.read_csv(data_csv,
-    #                    encoding='latin',
-    #                    dtype=str,
-    #                    na_values=[''],
-    #                    keep_default_na=False).dropna(how='all', axis=1)
-    # country = pd.read_csv(country_csv,
-    #                       encoding='latin',
-    #                       dtype=str,
-    #                       na_values=[''],
-    #                       keep_default_na=False).dropna(how='all', axis=1)
-    # series = pd.read_csv(series_csv,
-    #                      encoding='latin',
-    #                      dtype=str,
-    #                      na_values=[''],
-    #                      keep_default_na=False).dropna(how='all', axis=1)
+    data = pd.read_csv(data_csv,
+                       encoding='latin',
+                       dtype=str,
+                       na_values=[''],
+                       keep_default_na=False).dropna(how='all', axis=1)
+    country = pd.read_csv(country_csv,
+                          encoding='latin',
+                          dtype=str,
+                          na_values=[''],
+                          keep_default_na=False).dropna(how='all', axis=1)
+    series = pd.read_csv(series_csv,
+                         encoding='latin',
+                         dtype=str,
+                         na_values=[''],
+                         keep_default_na=False).dropna(how='all', axis=1)
 
     econs = pd.read_excel(
         groups_xls,
@@ -99,12 +167,67 @@ def main():
                            na_values=[''],
                            keep_default_na=False).dropna(how='all')
 
+    # domain
+    print('creating economy domain...')
     domains = pd.read_excel(domain_xls)
     all_entities = extrace_economy_entities(domains, groups)
-    eco_domain = EntityDomain(id='economy', entities=all_entities, props={'name': 'Economy'})
+    eco_domain = EntityDomain(id='economy',
+                              entities=all_entities,
+                              props={'name': 'Economy'})
     for eset in eco_domain.entity_sets:
-        df = pd.DataFrame.from_records([v.to_dict() for v in eco_domain.get_entity_set(eset)])
+        df = pd.DataFrame.from_records(
+            [v.to_dict() for v in eco_domain.get_entity_set(eset)])
         df.to_csv(f'../../ddf--entities--economy--{eset}.csv', index=False)
+
+    # datapoints
+    print('creating datapoints...')
+    datapoints = extract_datapoints_country_year(data)
+    datapoints_output_dir = os.path.join(output_dir, 'datapoints')
+    os.makedirs(datapoints_output_dir, exist_ok=True)
+    for k, v in datapoints.items():
+        v[k] = pd.to_numeric(v[k])
+        if not v.empty:
+            v.to_csv(
+                os.path.join(
+                    datapoints_output_dir,
+                    'ddf--datapoints--' + k + '--by--economy--year.csv'),
+                index=False,
+                encoding='latin',
+                # keep 10 digits. this is to avoid pandas
+                # use scientific notation in the datapoints
+                # and also keep precision. There are really
+                # small/big numbers in this datset.
+                float_format='%.10f')
+
+    # concepts
+    print('creating concepts files...')
+    concepts = list()
+    for c in extract_concept_series(series):
+        concepts.append(c)
+
+    # TODO: extract concepts from entities
+
+    concept_continuous = [
+        c.to_dict() for c in concepts if c.concept_type == 'measure'
+    ]
+    concept_continuous_df = pd.DataFrame.from_records(concept_continuous)
+    concept_continuous.to_csv(os.path.join(output_dir,
+                                           'ddf--concepts--continuous.csv'),
+                              index=False,
+                              encoding='latin')
+
+    concept_discrete = [
+        c.to_dict() for c in concepts if c.concept_type != 'measure'
+    ]
+    concept_discrete_df = pd.DataFrame.from_records(concept_discrete)
+    concept_discrete.to_csv(os.path.join(output_dir,
+                                         'ddf--concepts--discrete.csv'),
+                            index=False,
+                            encoding='latin')
+
+    # datapackage
+    dump_json(os.path.join(output_dir, 'datapackage.json'),
+              get_datapackage(output_dir, update=True))
 
 
 if __name__ == '__main__':
